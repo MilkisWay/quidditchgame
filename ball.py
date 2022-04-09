a487d18ae396f6ccadb66f679b295a8af169ef31
Milana Shadrina <milana611shadrina@gmail.com>
2022-04-10 00:43:57 +0300

    Part 5

--- ball.py
+++ ball.py
@@ -14,8 +14,8 @@ class BallController(object):
     def __init__(self):
         self.balls = []
 
-    def addBall(self,pos,speed):
-        self.balls.append(BallModel(pos,speed))
+    def addBall(self,pos_x,pos_y,speed):
+        self.balls.append(BallModel(pos_x,pos_y,speed))
 
     def fly():
         pass
@@ -26,49 +26,4 @@ class BallView(object):
         self.image = pygame.image.load(img)
     def render(self, surface):
         for i in self.BallController.balls:
-            surface.blit(self.image,self.BallController.balls[i].pos)
-
-
-class Quaffle(Ball):
-    def __init__(self, game):
-        Ball.__init__(self, game)
-        self.image = pygame.image.load("quaffle.png")
-        self.mask = pygame.mask.from_surface(self.image)
-        self.held_by = None
-
-    def update(self):
-        self._update()
-        DAMPING = 0.3
-        gravity_vec = Vec2d(0, GRAVITY).normalized()#whaaaaat&&
-        self.velocity += gravity_vec * 0.01
-
-        if self.held_by:
-            self.position = self.held_by.position.copy()
-        else:
-            self.position += self.velocity.normalized() * self.acceleration
-
-        if self.acceleration > 1:
-            self.acceleration = self.acceleration - DAMPING
-        elif self.acceleration < 1:
-            self.acceleration = 1
-
-    def draw(self):
-        if self.game.camera.onScreen(self):
-            local_x = self.position.x - self.game.camera.x
-            local_y = self.position.y - self.game.camera.y
-            self.game.screen.blit(self.image, (local_x, local_y))
-
-    def setPossession(self, player):
-        self.held_by = player
-
-    def getPossession(self):
-        return self.held_by
-
-    def throw(self, angle, power):
-        self.velocity = angle
-        self.acceleration = power
-        self.setPossession(None)
-    def distance(from_sprite, to_sprite):
-        return (to_sprite.position - from_sprite.position).get_length()
-
-
+            surface.blit(self.image,(i.pos.x,i.pos.y))
--- quaffle.py
+++ quaffle.py
@@ -4,6 +4,7 @@ import sys
 from pygame.locals import *
 from ball import *
 from mode import *
+from mapp import *
 
 class QuaffleModel(BallModel):
     def __init__(self, pos_x, pos_y, speed):
@@ -11,18 +12,24 @@ class QuaffleModel(BallModel):
      self.pos = pygame.math.Vector2(pos_x,pos_y)
      self.rect = pygame.Rect(self.pos.x,self.pos.y, 0, 0)
      self.speed = pygame.math.Vector2(speed,speed)
-     self.mass = Mode().mode
-     self.g=9.8
+     #self.mass = Mode().mode not right
 
 
 class QuaffleController():
-    def __init__(self):
-        self.ball
-
-    def initball(self,pos_x, pos_y, speed):
-        self.ball = QuaffleModel(pos_x, pos_y, speed)
+    def __init__(self,ball:QuaffleModel):
+        self.ball = ball
 
     def fly(self,time):#time from Chaserpower
         for i in range(1,time):
-            self.pos.x=self.pos.x+self.speed.x*i
-            self.pos.y=self.pos.y+self.speed.y*i - (self.g*(i**2))//2
+            self.ball.pos.x=self.ball.pos.x+self.ball.speed.x*i
+            self.ball.pos.y=self.ball.pos.y+self.ball.speed.y*i - (self.ball.g*(i**2))//2
+            if self.ball.pos.y <=0:
+                self.ball.pos.y = screen_height-100
+                self.ball.pos.x = screen_width//2
+            elif self.ball.pos.x <=0 and self.ball.pos.x>=1080:
+                self.ball.pos.x = screen_width//2
+                self.ball.pos.y = screen_height-100
+            #add stop if interaction with hunter
+    def got_caught(a):#add hunter
+        #call funtion from collision controller
+
