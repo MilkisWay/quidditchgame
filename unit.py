41e19325611600be6bb72ca5fb44cb3aa8157ad9
Milana Shadrina <milana611shadrina@gmail.com>
2022-04-20 22:12:46 +0300

    None

---
+++ unit.py
@@ -0,0 +1,22 @@
+import pygame
+from object import Object 
+import pygame.locals as local
+
+class Unit(Object):
+    def __init__(self, x,y,speed):
+        Object.__init__(self)
+        self.pos = pygame.math.Vector2(x,y)
+        self.speed = pygame.math.Vector2(0,0)
+
+class Unit_Controller:
+    def __init__(self,units):
+        self.units=[]
+
+
+class UnitView:
+    def __init__(self,Unit_Controller,image):
+        self.Unit_Controller = Unit_Controller
+        self.image = pygame.image.load(image)
+    def render(self, surface):
+        for i in self.Unit_Controller.units:
+            surface.blit(self.image,(i.get_Coord.x()))
