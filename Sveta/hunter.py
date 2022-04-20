2de025edc60fe5c5a71fa5e5a1c6478e4b0a5c58
Светлана Киселева <kiseleva03@inbox.ru>
2022-04-20 17:21:53 +0300

    update hunter

--- Sveta/hunter.py
+++ Sveta/hunter.py
@@ -2,48 +2,44 @@ import pygame
 import os
 import sys
 from player import Player
+from player import Player_controller
 from pygame.locals import *
 from ball import BallModel
 from collision import CollisionController
+from quaffle import QuaffleModel
 
 class Hunter(Player):
     def __init__(self,x,y,speed):
         Player.__init__(self,x,y,speed)
         
-        self.type = 'chaser'
+        self.type = 'hunter'
+        #self.team = team
         self.image = pygame.Surface((10, 10))
+   
         self.image=pygame.image.load('hunter.jpg')
+        self.image.set_colorkey('black')
         self.acceleration = 1
         self.power = 10
-        self.quaffle = []
+        self.quaffle = None
         self.pos = pygame.math.Vector2(30,10)
         self.speed=pygame.math.Vector2(0,0)
         self.rect = self.image.get_rect()
 
-class Hunter_controller:
+    def checkQuaffle(self, quaffle):
+        if self.collision_Detection(quaffle):
+            if quaffle.possession==False:
+                self.quaffle=quaffle
+                quaffle.possession=True
+                quaffle.pos.x=self.pos.x
+                quaffle.pos.y=self.pos.y
+                print('check')
+
+
+class Hunter_controller(Player_controller):
     def __init__(self,Hunter):
-        self.hunter=hunter
+        Player_controller.__init__(self)
+        self.hunter=Hunter
     
-    def update(self):
-        self.checkQuaffle()
-
-    def checkQuaffle(self):
-         if self.quaffle.get_possession_statues() is None:
-             if collision_Detection(self, self.quaffle):
-                self.quaffle.set_possession_statues(self)
-
-    def key_control(self, direction):
-            if direction=="left":
-                t=pygame.math.Vector2(-0.5, 0)
-            elif direction=="right":
-                t=pygame.math.Vector2(0.5, 0)
-            elif direction== "up":
-                t=pygame.math.Vector2(0, -0.5)
-            elif direction== "down":
-                t=pygame.math.Vector2 (0, 0.5)
-            self.acceleration += 0.9
-            self.speed+=t
-   
 class Hunter_View:
     def __init__(self, hunter_controller):
         self.hunter_controller=hunter_controller
